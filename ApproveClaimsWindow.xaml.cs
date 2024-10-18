59a110d69692ec7a10e653c31e938c71d7ffd0d4
ST10321833 <128794343+ST10321833@users.noreply.github.com>
2024-10-18 11:31:18 +0200

    Add files via upload

---
+++ ApproveClaimsWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace CMCS4
+{
+    public partial class ApproveClaimsWindow : Window
+    {
+      
+        public event Action ClaimsUpdated;
+
+        public ApproveClaimsWindow()
+        {
+            InitializeComponent();
+          
+            ClaimsListBox.ItemsSource = ClaimData.Claims.Select(c => new ClaimViewModel
+            {
+                Id = c.Id,
+                LecturerName = c.LecturerName,
+                HoursWorked = c.HoursWorked,
+                HourlyRate = c.HourlyRate,
+                Status = c.Status
+            }).ToList();
+        }
+
+        private void Approve_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClaimsListBox.SelectedItem is ClaimViewModel selectedClaim)
+            {
+                Claim originalClaim = ClaimData.Claims.First(c => c.Id == selectedClaim.Id);
+                originalClaim.Status = "Approved";
+                ClaimsListBox.Items.Refresh(); 
+                ClaimsUpdated?.Invoke(); 
+            }
+        }
+
+        private void Reject_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClaimsListBox.SelectedItem is ClaimViewModel selectedClaim)
+            {
+                Claim originalClaim = ClaimData.Claims.First(c => c.Id == selectedClaim.Id);
+                originalClaim.Status = "Rejected";
+                ClaimsListBox.Items.Refresh(); 
+                ClaimsUpdated?.Invoke(); 
+            }
+        }
+
+       private void Close_Click(object sender, RoutedEventArgs e)
+{
+    this.Close();
+}
+
+    }
+}
---
+++ SubmitClaimWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CMCS4;
+using System;
+using System.Windows;
+
+namespace CMCS4
+{
+    public partial class SubmitClaimWindow : Window
+    {
+        public SubmitClaimWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var newClaim = new Claim
+            {
+                LecturerName = LecturerNameTextBox.Text,
+                HoursWorked = double.Parse(HoursWorkedTextBox.Text),
+                HourlyRate = double.Parse(HourlyRateTextBox.Text),
+                Notes = NotesTextBox.Text
+            };
+
+       
+            ClaimData.Claims.Add(newClaim);
+
+            
+            this.Close();
+        }
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
